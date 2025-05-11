d3aa4f06c312167205aacb234cd17570c523ecf8
nara911 <n.amarsanaa@cqumail.com>
2025-05-11 21:56:09 +1000

    Assests updated

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -14,6 +14,29 @@ class CreateAdminUserSeeder extends Seeder
 {
     public function run(): void
     {
+		$password = "Caretaker@321";
+		$hashPassword = Hash::make($password);
+		$encryptedPassword = Crypt::encrypt($password);
+
+        $data = [
+			'first_name' => "Caretaker",
+			'last_name' => "User",
+			'email' => "caretaker@example.com",
+			'password' => $hashPassword,
+			'email_verified_at' => now(),
+			'cognifit_user_token' => null,
+			'secret_password' => $encryptedPassword,
+			'dob' => now()->toDateString()
+		];
+		if(!User::whereEmail($data['email'])->exists()) {
+			$user = User::create($data);
+			$user->assignRole(UserRolesEnum::CARETAKER->value);
+			UserDetail::create([
+				'user_id' => $user->id,
+				'status' => UserStatusEnum::ACTIVE->value,
+			]);
+		}
+
 		$password = "Admin@321";
 		$hashPassword = Hash::make($password);
 		$encryptedPassword = Crypt::encrypt($password);
@@ -36,6 +59,8 @@ class CreateAdminUserSeeder extends Seeder
 				'status' => UserStatusEnum::ACTIVE->value,
 			]);
 		}
+
+		
 
     }
 }
